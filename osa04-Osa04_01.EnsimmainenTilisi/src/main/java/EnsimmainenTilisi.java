d9a0afcb215f46a125af0a126a01564d31495149
Kai Jukarainen <100445882+Kaitsuus@users.noreply.github.com>
2022-08-18 15:51:28 +0300

    Add files via upload

---
+++ osa04-Osa04_01.EnsimmainenTilisi/src/main/java/EnsimmainenTilisi.java
@@ -0,0 +1,12 @@
+import javax.crypto.SealedObject;
+
+public class EnsimmainenTilisi {
+
+    public static void main(String[] args) {
+        // Tili.Java:ssa olevaan koodiin ei tule koskea
+        // tee ohjelmasi tänne
+        Tili uusiTili = new Tili("Uusi tili",100);
+        uusiTili.pano(20);
+        System.out.println(uusiTili);
+    }
+}
