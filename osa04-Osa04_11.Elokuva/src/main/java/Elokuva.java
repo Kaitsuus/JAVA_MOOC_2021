71774fb944153071f1e5999095e4357210fece29
Kai Jukarainen <100445882+Kaitsuus@users.noreply.github.com>
2022-08-19 13:14:26 +0300

    Add files via upload

---
+++ osa04-Osa04_11.Elokuva/src/main/java/Elokuva.java
@@ -0,0 +1,16 @@
+public class Elokuva {
+    private String nimi;
+    private int ikaraja;
+
+    public Elokuva(String elokuvanNimi, int elokuvanIkaraja) {
+        this.nimi = elokuvanNimi;
+        this.ikaraja = elokuvanIkaraja;
+    }
+    public String nimi() {
+        return nimi;
+    }
+    public int ikaraja() {
+        return ikaraja;
+    }
+    
+}
