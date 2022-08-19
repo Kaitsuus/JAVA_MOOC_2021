e3d9345964f944e6634caadd4b416f7935ec4fe2
Kai Jukarainen <100445882+Kaitsuus@users.noreply.github.com>
2022-08-19 13:12:08 +0300

    Add files via upload

---
+++ osa04-Osa04_07.Tuote/src/main/java/Tuote.java
@@ -0,0 +1,15 @@
+public class Tuote {
+    private String nimi;
+    private double hinta;
+    private int maara;
+
+    public Tuote(String nimiAlussa, double hintaAlussa, int maaraAlussa) {
+        this.nimi = nimiAlussa;
+        this.hinta = hintaAlussa;
+        this.maara = maaraAlussa;
+    }
+    public void tulostaTuote() {
+        System.out.println(this.nimi + ", hinta " + this.hinta + ", " + this.maara + " kpl");
+    }
+    
+}
---
+++ osa04-Osa04_09.Velka/src/main/java/Velka.java
@@ -0,0 +1,19 @@
+import javax.annotation.processing.SupportedOptions;
+import javax.sound.sampled.SourceDataLine;
+
+public class Velka {
+    private double saldo;
+    private double korkokerroin;
+
+    public Velka(double saldoAlussa, double korkokerroinAlussa) {
+        this.saldo = saldoAlussa;
+        this.korkokerroin =korkokerroinAlussa;
+    }
+    public void tulostaSaldo() {
+        System.out.println(this.saldo);
+    }
+    public void odotaVuosi() {
+        this.saldo = this.saldo*this.korkokerroin;
+    }
+    
+}
